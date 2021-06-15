6ff44750f7dd0e9352ce0a2eb78f96a197f54a52
闫长辉 <huizaierr@163.com>
2021-06-15 23:46:10 +0800

    1.第二次提交

--- app/src/main/java/com/ych/hi_library/MainActivity.kt
+++ app/src/main/java/com/ych/hi_library/MainActivity.kt
@@ -2,10 +2,12 @@ package com.ych.hi_library
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tv_test.text = "张三！！！"
     }
 }
