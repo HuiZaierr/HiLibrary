5c5bda162189a6a873d393ab95cbe1018af370d6
闫长辉 <huizaierr@163.com>
2021-07-27 08:10:09 +0800

    1.第二次提交

--- app/src/main/java/com/ych/hi_library/SecondActivity.java
+++ app/src/main/java/com/ych/hi_library/SecondActivity.java
@@ -1,6 +1,8 @@
 package com.ych.hi_library;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -8,18 +10,28 @@ import android.widget.TextView;
 
 import com.ych.hilibrary.manager.ActivityManager;
 
-public class SecondActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class SecondActivity extends AppCompatActivity {
+    private ArrayList<String> list = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         TextView textView = findViewById(R.id.tv_text);
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        for (int i = 0; i < 20; i++) {
+            list.add(String.valueOf(i));
+        }
 
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+//        recyclerView.setAdapter();
        Activity activity = ActivityManager.Companion.getInstance().getTopActivity();
        if (activity!=null){
            textView.setText(activity.getLocalClassName());
        }
+
+
 
     }
 }
