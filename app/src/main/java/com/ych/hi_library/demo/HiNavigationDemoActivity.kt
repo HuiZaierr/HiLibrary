10cc927984263f4386c72839a4d176f010cba1bd
闫长辉 <huizaierr@163.com>
2021-08-04 08:09:56 +0800

    1.第二次提交

---
+++ app/src/main/java/com/ych/hi_library/demo/HiNavigationDemoActivity.kt
@@ -0,0 +1,15 @@
+package com.ych.hi_library.demo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ych.hi_library.R
+
+class HiNavigationDemoActivity:AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hi_navigation_demo)
+    }
+
+
+}
