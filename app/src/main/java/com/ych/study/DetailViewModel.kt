30c41c5116ddca0f686c0a8677ba2e6c01e30fa3
yanchanghui <huizaierr@163.com>
2022-06-22 18:29:00 +0800

    1.添加Material的Chip

--- app/src/main/java/com/ych/study/DetailViewModel.kt
+++ app/src/main/java/com/ych/study/DetailViewModel.kt
@@ -2,6 +2,8 @@ package com.ych.study
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStoreOwner
+import com.ych.base.network.response.base.BaseResponse
 
 /***
  * TODO：如果在ViewModel构造函数中传递参数，那么需要我们创建Factory来实例化ViewModel。
@@ -11,17 +13,29 @@ import androidx.lifecycle.ViewModelProvider
 class DetailViewModel(val goodsId:String):ViewModel() {
 
     companion object{
-        private class DetailViewModelFactory(val goodsId:String):ViewModelProvider.NewInstanceFactory(){
+        private class DetailViewModelFactory(val goodsId:String?):ViewModelProvider.NewInstanceFactory(){
             /**
              * TODO：重写creat方法
              */
             override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-                val constructor = modelClass.getConstructor(String::class.java)
-                if (constructor!=null){
-                    return constructor.newInstance(goodsId)
+                //如果构造方法没有传入goodsId,那么会报错。
+                try {
+                    val constructor = modelClass.getConstructor(String::class.java)
+                    if (constructor!=null){
+                        return constructor.newInstance(goodsId)
+                    }
+                }catch (e:Exception){
+                    //ignore
                 }
                 return super.create(modelClass)
             }
+        }
+
+        /**
+         * TODO；提供一个方法获取DetailViewModel对象。
+         */
+        fun get(goodsId:String?,viewModelStoreOwner: ViewModelStoreOwner):DetailViewModel{
+            return ViewModelProvider(viewModelStoreOwner,DetailViewModelFactory(goodsId)).get(DetailViewModel::class.java)
         }
     }
 }
